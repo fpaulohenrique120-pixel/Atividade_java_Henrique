4f19034bdc93c4e2cf7642778fe412a05414eb8f
fpaulohenrique120-pixel <fpaulohenrique120@gmail.com>
2025-10-24 11:13:29 -0300

    Add files via upload

---
+++ questao9.java
@@ -0,0 +1,14 @@
+public class questao9 {
+    public static void main(String[] args) {
+        double nota1=8.5;
+        double nota2=9.0;
+        double nota3=9.5;
+        double media=(nota1 + nota2 + nota3)/3;
+        if (media >= 7) {
+            System.out.println("aprovado");
+        }
+else {
+    System.out.println("reprovado");
+}
+    }
+}
