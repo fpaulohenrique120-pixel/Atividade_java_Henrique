be7ee52b1a4a4d385aedcf013f271e52dc2783cb
fpaulohenrique120-pixel <fpaulohenrique120@gmail.com>
2025-10-24 11:00:59 -0300

    Add files via upload

---
+++ Informaçoespessoais.java
@@ -0,0 +1,9 @@
+public class Informaçoespessoais {
+    public static void main(String[] args) {
+        String nome = "Miguel";
+        int idade = 15;
+        double altura = 1.80;
+
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e minha altura é " + altura);
+}
+}
