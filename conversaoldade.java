fcf504df12685d7154d9a01f38afe756efff72c7
fpaulohenrique120-pixel <fpaulohenrique120@gmail.com>
2025-10-24 10:59:06 -0300

    Add files via upload

---
+++ conversaoldade.java
@@ -0,0 +1,11 @@
+public class conversaoldade {
+    public static void main(String[] args) {
+        int idade = 15;
+
+        int meses = idade * 12;
+        int dias = idade * 365;
+
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Equivale a " + meses + " meses ou " + dias + " dias");
+}
+ }
