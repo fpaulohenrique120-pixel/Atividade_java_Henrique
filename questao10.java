fe33832d33b0c2e3334366686a85202e473679f2
fpaulohenrique120-pixel <fpaulohenrique120@gmail.com>
2025-10-24 11:20:33 -0300

    Add files via upload

---
+++ questao10.java
@@ -0,0 +1,9 @@
+public class questao10 {
+    public static void main(String[] args) {
+        int idade=16;
+        int meses=idade*12;
+        int dias=idade*365;
+        System.out.println("idade" + idade  + "anos");
+        System.out.println("Equivale a:" + meses + "meses" + dias + "dias");
+    }
+}
